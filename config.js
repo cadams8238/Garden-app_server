60c7a32682bf3bc2fc5ce396502fc4515bec4822
Courtney Adams <cadams8238@gmail.com>
2018-07-05 14:10:07 -0700

    changed mongoDB uri, old one was being buggy and not authorizing users

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user:password1@ds125831.mlab.com:25831/garden-app',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user:password1@ds127811.mlab.com:27811/garden-app',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   WEATHER_API_KEY: process.env.WEATHER_API_KEY,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+  // DATABASE_URL:
+  //       process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  // TEST_DATABASE_URL:
+  //       process.env.TEST_DATABASE_URL ||
+  //       'mongodb://localhost/thinkful-backend-test'
 };
