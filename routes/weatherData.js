9fb93cfb38da44ce818619b3eff9b464765fd662
Courtney Adams <cadams8238@gmail.com>
2018-07-09 10:35:51 -0700

    updated weatherData endpoint to receive zipcode and request weather data from api based on zipcode

--- routes/weatherData.js
+++ routes/weatherData.js
@@ -3,26 +3,30 @@ const router = express.Router();
 const fetch = require('isomorphic-fetch');
 
 const { WEATHER_API_KEY } = require('../config');
+//forecast 5day 3hr
 // const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=Portland&mode=json&APPID=';
-//current weather 3hr forecast
-const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=97211&mode=json&APPID=';
-// const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=97211';
-// const WEATHER_API_URL_ENDING = '&mode=json&APPID=';
+//current weather 3hr
+// const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=97211&mode=json&APPID=';
+
+const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const WEATHER_API_URL_ENDING = '&mode=json&APPID=';
 
 // router.get('/testData', (req, res, next) => {
 //     res.send({data: 'hello'})
 // })
 
-// fetch(`${WEATHER_API_URL}${zipcode}${WEATHER_API_URL_ENDING}${WEATHER_API_KEY}`)
+// fetch(`${WEATHER_API_URL}${WEATHER_API_KEY}`)
 
 
-router.get('/', (req, res, next) => {
-    fetch(`${WEATHER_API_URL}${WEATHER_API_KEY}`)
-    .then(response => response.json())
-    .then(jsonData => {
-        res.json(jsonData);
-    })
-    .catch(err => next(err));
+router.get('/:zipcode', (req, res, next) => {
+    const { zipcode } = req.params;
+
+    fetch(`${WEATHER_API_URL}${zipcode}${WEATHER_API_URL_ENDING}${WEATHER_API_KEY}`)
+        .then(response => response.json())
+        .then(jsonData => {
+            res.json(jsonData);
+        })
+        .catch(err => next(err));
 })
 
 module.exports = router;
